fdf6fbf1ed45ed99ddfb63bbfea600ca5eee3481
gera <gera.weiss@gmail.com>
2025-06-03 11:00:42 +0300

    Seperate test cases for linear and parallel modes in BookStoreDemo

---
+++ BookStoreDemo/spec/js/_mode_selection.js
@@ -0,0 +1,16 @@
+//@provengo summon rest
+
+// Temporary workaround for selecting the mode of operation in the BookStoreDemo test suite.
+// Ideally, this should be handled cleanly via a dedicated command or a built-in API feature,
+// but such a mechanism is either not available or not yet known to me (Gera).
+// Supported modes are "LINEAR" and "PARALLEL".
+// The mode can be specified using the option `-c mode=<mode>` when running the test.
+
+
+// "LINEAR" or "PARALLEL"
+with (Main.options.getOptional('mode', Packages.java.lang.String)) {
+  const mode = isPresent() ? get() : "LINEAR"; // Default to LINEAR if not specified
+}
+
+bp.log.info("Starting BookStoreDemo test with mode: " + mode);
+
---
+++ BookStoreDemo/spec/js/a_linear_test.js
@@ -0,0 +1,17 @@
+//@provengo summon rest
+
+// Parse this file only if the mode is "LINEAR"
+if(mode == "LINEAR") {
+
+// A simple linear story for a library loan system
+bthread("Loan API", function () {
+  addUser(111, "John Doe");
+  addBook(222, "The Great Gatsby");
+  addLoan(111, 222);
+  tryToAddExistingLoan(111, 222);
+  verifyLoanExists(111, 222);
+  verifyUserExists(111, "John Doe");
+  verifyBookExists(222, "The Great Gatsby");
+});
+
+} // End of "LINEAR" mode
