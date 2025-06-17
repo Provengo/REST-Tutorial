96c83012a321f1d1e45b7ebba98bb72f55730d8a
Gera Weiss <gera.weiss@gmail.com>
2025-06-17 07:52:44 +0300

    Improved the explnation

--- run_test.sh
+++ run_test.sh
@@ -4,6 +4,6 @@
 #  provengo              : The model-based testing tool
 #  run                   : Run the test model
 #  REST-Tutorial/BookStoreDemo : Location of the test model
-#  --before=REST-Tutorial/reset.sh : Run reset.sh before each test to clean database
+#  --before=REST-Tutorial/reset.sh : Run reset.sh before each test to reset the database
 #  | sed ...            : Clean up output by extracting only the step description
 provengo run REST-Tutorial/BookStoreDemo --before=REST-Tutorial/reset.sh | sed -E 's/(.*) Selected: \[.*description:"([^"]+).*/\1 Selected: [\2]/'
